b7e7cec8cef3298c8d8d0bf1f24b56bb8ff41f05
Ezequiel Gonzalez <Ezequiel Gonzalez@DESKTOP-FEH2C4B>
2024-04-12 16:03:03 -0300

    Desarrollo ejercicio 2

---
+++ tp1_GonzalezLopezAlfredo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "España";
+		int edad = 26;
+		float alturaEdificio = 171.8f;
+		float precioProducto = 542.4f;
+		double numeroTelefono = 3884775847d;
+		float coseno = 0.5f;
+		System.out.println(pais);
+		System.out.println(edad);
+		System.out.println(alturaEdificio);
+		System.out.println(precioProducto);
+		System.out.println(numeroTelefono);
+		System.out.println(coseno);
+	}
+
+}
