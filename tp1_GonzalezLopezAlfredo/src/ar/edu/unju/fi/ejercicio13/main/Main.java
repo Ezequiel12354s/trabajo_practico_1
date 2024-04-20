a8deb32382516ea6264ac3a5ab26e1811777e74d
Ezequiel Gonzalez <Ezequiel Gonzalez@DESKTOP-FEH2C4B>
2024-04-20 17:52:07 -0300

    Desarrollo ejercicio 13

---
+++ tp1_GonzalezLopezAlfredo/src/ar/edu/unju/fi/ejercicio13/main/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio13.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int vector [] = new int[8];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=1;i<8;i++) {
+			System.out.println("Ingrese un numero: ");
+			vector[i]=sc.nextInt();
+			System.out.println("Numero: "+vector[i]+" posicion "+i);
+		}
+		sc.close();
+	}
+
+}
